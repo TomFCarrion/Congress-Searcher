44d2a8e075238134f3ad181295dea9a62d9970a3
Tomas F. Carrion <tomfcarrion@gmail.com>
2019-12-14 20:38:42 -0300

    feature/infocard: add dinamic image

--- src/components/infoCard/index.js
+++ src/components/infoCard/index.js
@@ -3,13 +3,29 @@ import React from 'react'
 import './styles.scss'
    
 
-const DEFAULT_IMAGE = 'https://drogaspoliticacultura.net/wp-content/uploads/2017/09/placeholder-user.jpg'
+const REPUBLICAN_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Republican_Disc.svg/1200px-Republican_Disc.svg.png'
+const DEMOCRAT_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/US_Democratic_Party_Logo.svg/2000px-US_Democratic_Party_Logo.svg.png'
+const INDEPENDENT_IMAGE = 'https://pbs.twimg.com/profile_images/520616958006267904/fhZzscao_400x400.png'
+export const InfoCard = ({ id, party='', fullName = 'Full Name', title = 'senator'}) => {
 
-export const InfoCard = ({ id, fullName = 'Full Name', title = 'senator', src = DEFAULT_IMAGE }) => {
+  let PartyImage = () => {
+    if (party === "D") {
+       return DEMOCRAT_IMAGE
+    } 
+    if (party === "R") {
+      return REPUBLICAN_IMAGE
+    } 
+
+    if (party === "ID") {
+      return INDEPENDENT_IMAGE
+    } 
+  
+  }
+
   return (
     <article>
       <div className="container">
-           <img className="image" src={src} alt="member"/>
+           <img className="image" src={PartyImage()} alt="member"/>
            <div className="info">
                 <span className="primary">{ fullName }</span>
                 <span className="secondary">{ title }</span>
