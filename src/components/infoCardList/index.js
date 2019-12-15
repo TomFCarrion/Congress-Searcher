a34bf1843f3cfd856d1e530003e42dd547386ab2
Tomas F. Carrion <tomfcarrion@gmail.com>
2019-12-15 01:57:56 -0300

    feature/infoCardList: add info card List

---
+++ src/components/infoCardList/index.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react'
+import { InfoCard } from '../infoCard/index';
+
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+
+export const InfoCardList = () => {
+
+    const content = useSelector(state => state.membersReducer);
+    const dispatch = useDispatch();
+
+    let config = {
+        headers:{
+            'X-API-KEY': ''
+        }
+    }
+
+    function fetchData() {
+        return dispatch => {
+          axios.get('https://api.propublica.org/congress/v1/116/senate/members.json',  config
+        )
+          .then(res =>
+            dispatch({
+              type: "FETCH_MEMBERS",
+              data: res.data.results[0].members
+            })
+          );
+        };
+      }
+
+        useEffect(() => {
+            dispatch(fetchData());
+        }, [])
+
+      const filter = () => {  
+       let filter = []
+       let advanceSearch = "";
+
+
+       Object.values(content.data).forEach(e => {
+        
+        switch(content.searchType) {
+          case "name":
+            advanceSearch = `${e.first_name}${e.last_name}`;
+            break;
+          case "title":
+            advanceSearch = e.title;
+            break;
+          case "gender":
+              advanceSearch = e.gender;
+            break;
+            case "party":
+              advanceSearch = e.party;
+            break;
+              
+          default:
+            break;
+         }
+
+          if (advanceSearch.toLowerCase().indexOf(content.inputSearch) >= 0) {
+            return filter.push(e);
+          }
+        });
+        return filter;
+
+      }
+
+    return (
+        <ul>
+            {filter().map(member => <InfoCard  party={member.party} fullName={`${member.first_name} ${member.last_name}`} title={member.title} key={member.id}/> )}
+        </ul>     
+    )
+}
